fdb00543b5e7989bd76f77935538e57c26ed48a2
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2026-04-17 18:26:44 -0400

    Diag

---
+++ examples/diag.asm
@@ -0,0 +1,21 @@
+.macro diag src tmp1 tmp2
+@loop:
+  case src
+  goto @moveback
+  goto @one
+  ;; hash
+  add 1## tmp1
+  add # tmp2
+  goto @loop
+
+@one:
+  add 1# tmp1
+  add 1 tmp2
+  goto @loop
+
+@moveback:
+  move tmp1 src
+  move tmp2 src
+.endmacro
+
+diag R1 R2 R3
